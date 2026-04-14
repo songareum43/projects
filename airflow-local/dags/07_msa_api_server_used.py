3867cbb83fdca7f37c79997c547e7f7cd7c84a9d
songareum <songlove0403@gmail.com>
2026-04-14 09:29:22 +0900

    change

--- airflow-local/dags/07_msa_api_server_used.py
+++ airflow-local/dags/07_msa_api_server_used.py
@@ -13,18 +13,51 @@ import requests # api 호출용, MSA 서비스 호출용
 # 2. API 서버 주소
 API_URL = 'http://127.0.0.1:8000/predict'
 
+# 4-4. 콜백 함수 정의
+def _create_dummy_data(**kwargs):
+    pass
+
+
+def _api_service_call(**kwargs):
+    pass
+
+def _load_users_credit(**kwargs):
+    pass
+
+
 # 3. DAG 정의
 with DAG(
-
+    dag_id = '07_msa_aoi_server_used', 
+    description = "MSA 상에 특정 서비스(ai 서빙 컨셉)를 호출하여 신용 평가 수행하는 스케줄링", 
+    default_args = {
+        'owner'          : 'de_2team_manager',
+        'retries'        : 1, 
+        'retry_delay'    : timedelta(minutes=1)
+    }, 
+    schedule_interval = '@daily', # 분 시 ... => 매일 오전 09시 00분 스케줄 작동
+    start_date = datetime(2026,2,25),                     
+    catchup = False, 
+    tags = ['msa', 'fastapi']
 ) as dag:
     # 4. Task 정의
     # 4-1. 더미 데이터 준비 -> 추후 고객 정보 저장 -> 추후 s3 업로드
-    task_create_dummy_data = PythonOperator()
+    task_create_dummy_data = PythonOperator(
+        task_id = "task_create_dummy_data",
+        python_callable= _create_dummy_data
+    )
+
     # 4-2. API 호출(AI 서비스 활용) -> 신용 평가 획득
-    task_ai_service_call = PythonOperator()
+    task_api_service_call = PythonOperator(
+        task_id = "task_api_service_call",
+        python_callable=_api_service_call
+    )
+
     # 4-3. 결과 저장 -> 추후 고객 정보 업데이트
-    task_load_users_credit = PythonOperator()
+    task_load_users_credit = PythonOperator(
+        task_id = "task_load_users_credit",
+        python_callable=_load_users_credit
+    )
     
     # 5. 의존성, 각 task는 xcom 통신으로 데이터 공유
-    task_create_dummy_data >> task_ai_service_call >> task_load_users_credit
+    task_create_dummy_data >> task_api_service_call >> task_load_users_credit
     
