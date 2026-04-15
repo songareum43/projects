dc30286de1ae4a3658bc0b3f95c9d5e479f87b60
songareum <songlove0403@gmail.com>
2026-04-15 13:51:46 +0900

    Feat: 로그 생성 코드 최종

--- airflow-local/log_gen/run.py
+++ airflow-local/log_gen/run.py
@@ -7,21 +7,39 @@ import time
 # 현재 워킹디렉토리에서 코드를 작동할 때 경로
 from log_generator import LogGenerator
 
-log_gen = LogGenerator() 
+log_gen = LogGenerator()
+log_gen_map={
+   "finance":log_gen.finance(),
+   "factory":log_gen.factory() # 리뷰 때 추가 완성
+} 
 
-def make_log():
-  print(f'finance 로그 생성 시작')
-  print('-'*50)
-  for i in range(50):
-    log = log_gen.finance()
-    # ensure_ascii 이스케이프 문자들 변환없이 있는 그대로 출력하는가?
-    log_json = json.dumps(log, ensure_ascii=False)
-    print(f'[Log-{i+1}]{log_json}')
-    # 다음 로그 발생까지 대기시간, 동시간에 n개 로그 x
-    # 다른 시간대에 1개의 로그 형태임 -> 컨셉에 따라 다르게 구성 가능
-    time.sleep(log_gen.get_interval_time('random', 1))
-    # break
-  print('-'*50)
+def make_log(config):
+    log_gen = LogGenerator()
+    log_gen_map={
+   "finance":log_gen.finance, # 함수 주소값 세팅
+   "factory":log_gen.factory, # 리뷰 때 추가 완성
+} 
+    print(f'{config["target_industry"]} 로그 생성 시작')
+    print('-'*50)
+    for i in range(config["total_count"]): # loop 옵션은 리뷰 때 수정 시도
+    # log = log_gen.finance
+        cur_func = log_gen_map.get(config['target_industry'])
+        log = cur_func()
+        # ensure_ascii 이스케이프 문자들 변환없이 있는 그대로 출력하는가?
+        log_json = json.dumps(log, ensure_ascii=False)
+        print(f'[Log-{i+1}]{log_json}')
+        # 다음 로그 발생까지 대기시간, 동시간에 n개 로그 x
+        # 다른 시간대에 1개의 로그 형태임 -> 컨셉에 따라 다르게 구성 가능
+        time.sleep(log_gen.get_interval_time(config['mode'], config['interval'] ))
+        # break
+    print('-'*50)
 
 if __name__=="__main__":
-    make_log()
+    config = {
+       "target_industry":"finance", # finance, IOT..., game_lol
+       "mode":"random", # random or fixed
+       "interval":1, # 초 단워
+       "total_count":10, # 생성 개수
+       "loop":False # 무한대 생성, 작동 False
+    }
+    make_log(config)
