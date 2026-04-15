ac4514d911272a6cd5418c067bb1ac6b9d6d48c0
songareum <songlove0403@gmail.com>
2026-04-15 15:19:56 +0900

    Feat: adf 데이터 전송 코드 기본

--- airflow-local/log_gen/adf_direct_data_put.py
+++ airflow-local/log_gen/adf_direct_data_put.py
@@ -5,3 +5,25 @@
 import boto3
 import json
 import time
+
+# 2. 환경변수
+ACCESS_KEY = ''
+SECRET_KEY = ''
+REGION = 'ap-northeast-2'
+
+# 3. 특정 서비스(ADF) 클라이언트 생성
+# AWS 외부에서 진행
+def get_client(service_name='firehose', is_in_aws=True):
+    if not is_in_aws:
+        session = boto3.Session(
+            aws_access_key_id = ACCESS_KEY,
+            aws_secret_access_key = SECRET_KEY,
+            region_name = REGION
+        )
+        return session.client('firehose')
+
+    # AWS 내부에서 진행
+    return boto3.client('firehose', region_name = REGION)
+
+firehose = get_client()
+print(firehose)
