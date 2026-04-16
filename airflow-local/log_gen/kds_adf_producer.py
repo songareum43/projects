e14417d231c437bb5aaf52a176d8947576234ee4
songareum <songlove0403@gmail.com>
2026-04-16 10:37:17 +0900

    Feat: 키네스

--- airflow-local/log_gen/kds_adf_producer.py
+++ airflow-local/log_gen/kds_adf_producer.py
@@ -21,3 +21,56 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 REGION = 'ap-northeast-2'
 
 print(ACCESS_KEY, SECRET_KEY)
+
+# 3. 클라이언트 생성
+def get_client(service_name='firehose', is_in_aws=True):
+    if not is_in_aws:
+        session = boto3.Session(             # session : 환경 설정?! => 정보 담기
+            aws_access_key_id = ACCESS_KEY,
+            aws_secret_access_key = SECRET_KEY,
+            region_name = REGION
+        )
+        return session.client('firehose') # client : 전용 제어판?! => 명령 내릴 프로그램 선택
+
+    # AWS 내부에서 진행
+    return boto3.client('firehose', region_name = REGION)
+
+kinesis = get_client('kinesis', False)
+print(kinesis)
+
+# 4. 데이터 제너레이터 함수 구성
+def gen_stock_data():
+    ticker = ['NVDA', 'GOOGL', 'AAPL', 'TSLA', 'AMZN', 'MSFT']
+    # 종목별 특정 시간(기간)동안 평균가 연산 => s3 전달 목표
+    return {
+        "event_time":datetime.now().isoformat,
+        "ticker": random.choice(ticker),
+        "price":round(random.uniform(100,1000), 2),
+        "volume":random.randint(1,100),
+        "trade_id":random.randint(100000,999999)
+    }
+
+# 5. 데이터 전송
+print('stock 거래 데이터 전송 시작...')
+try:
+    while True:
+        # 데이터 생성
+        data = gen_stock_data()
+        print(f'전송 전:{data}')
+        # kinesis 전달
+        kinesis.put_record(
+            # 스트림 이름
+            StreamName = 'de-ai-09-an2-kds-stock-analysis', 
+            # 데이터(객체 직렬화하여 문자열 제공)
+            Data = json.dumps(data),
+            # 티커별로 샤드(고속도로의 차선) 분산하여 kinesis에 전달
+            # 티커가 6개이므로 샤드를 6개(6차선 도로를 구성, 6개의 줄기를 구성)로 하여 개별 데이터 전송
+            PatitionKey = data['ticker'] # 해당 컬럼의 고유값의 개수만큼 조각(샤드, 전용 차선) 구성
+        )
+        # 로그
+        print(f"전송:{data}")
+        # 잠시 대기
+        time.sleep(0.5) # 0.5초 대기
+        break
+except Exception as e:
+    print('중단', e)
