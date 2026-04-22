1010733dbf70c197153b50b22546dc0456fc3fd7
songareum <songlove0403@gmail.com>
2026-04-22 13:38:08 +0900

    Feat : DAG 구성 완료

--- airflow-local/dags/11_aws_ma_silver.py
+++ airflow-local/dags/11_aws_ma_silver.py
@@ -24,8 +24,8 @@ from airflow import DAG
 from airflow.providers.amazon.aws.operators.athena import AthenaOperator
 
 # 2. 환경변수
-DATABASE_BRONZE = 'de-ai-09-ma-bronze-db'
-DATABASE_SILVER = 'de-ai-09-ma-silver-db'
+DATABASE_BRONZE = 'de_ai_09_ma_bronze_db'
+DATABASE_SILVER = 'de_ai_09_ma_silver_db'
 SILVER_S3_PATH = 's3://de-ai-09-827913617635-ap-northeast-2-an/medallion/silver/'
 ATHENA_RESULTS = 's3://de-ai-09-827913617635-ap-northeast-2-an/athena-results/'
 SILVER_TBL_NAME = 'sales_silver_tbl'
@@ -42,7 +42,7 @@ with DAG(
     schedule_interval = '10 * * * *', 
     start_date = datetime(2026,2,25),                     
     catchup = False, 
-    tags = ['aws', 'medallion', 'silver', 'athena', 'ctas'] # ctas 조회를 바탕으로 테이블 생성
+    tags = ['aws', 'medallion', 'silver', 'athena', 'ctas'] 
 ) as dag:
 
     # 4. TASK 정의
@@ -54,8 +54,40 @@ with DAG(
         params = {'database_silver':DATABASE_SILVER, 'tbl_nm':SILVER_TBL_NAME}
     )
 
+    # 수행시간 => airflow context에 정보가 기록되어 있음
+    # Jinja 템플릿 활용중 => {{execution_date.format('YYYY')}} => 2026 세팅됨
     ctas_silver_task = AthenaOperator(
-        task_id = 'ctas_silver'
+        task_id = 'ctas_silver',
+        query='''
+            Create Table if not exists {{params.database_silver}}.{{params.tbl_nm}}
+            with(
+                format = 'PARQUET',
+                parquet_compression = 'SNAPPY',
+                external_location = {{params.silver_path}},
+                pattitionde_by = ARRAY['dt','hr']
+            )
+            As
+            Select event_id,
+            event_time as event_time_timestamp,
+            data.user_id,
+            data.item_id,
+            data.price,
+            data.qty, 
+            (data.price*data.qty) as total_price,
+            data.store_id,
+            source_ip,
+            user_agent,
+            cast(year || '-' || month || '-' || day as VARCHAR), as dt,
+            hour as hr
+            from {{params.DATABASE_BRONZE}}.raw_bronze_tbl
+            where   year = {{execution_date.format('YYYY')}}
+                and month = {{execution_date.format('MM')}}
+                and day = {{execution_date.format('DD')}}
+                and hour = {{execution_date.format('HH')}}
+
+        ''',
+        database = DATABASE_SILVER,
+        params = {'database_silver':DATABASE_BRONZE, 'database_silver':DATABASE_SILVER, 'tbl_nm':SILVER_TBL_NAME, 'silver_path':ATHENA_RESULTS}
     )
 
     # 5. 의존성
