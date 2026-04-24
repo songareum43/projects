c5551f4f0470d5480977d3e357a7242396ab79e7
songareum <songlove0403@gmail.com>
2026-04-24 10:40:34 +0900

    Feat : 로그 구성 완료

--- kafka-local/log_gen.py
+++ kafka-local/log_gen.py
@@ -16,11 +16,39 @@ if not os.path.exists(log_dir): # 없으면 생성
 
 # 3. 로그 발생 및 저장
 def generate_logs():
-    pass
+    # 로그 샘플(고정된 장비 1대로 지정)
+    # 편의상 시간을 제외한 모든 값 고정
+    data = {
+        "timestamp" : datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "sensor_id" : "AI-FACTORY-001",
+        "temperature" : 87.5,
+        "humidity" : 42.1,
+        "status" : "RUNNING",
+    }
+    # json 형태로 파일 기록(한줄에 로그 1개씩) -> dict 객체의 직렬화
+    # 파일명 ./sensor_logs/sensor_json.log
+    # 한줄에 JSON 객체 1개씩 문자열로 기록(JSON Lines : JSONL)
+    # a : append -> 파일에 추가 쓰기 모드 -> 파일에 끝에 새로운 내용 덧붙임
+    with open(f"{log_dir}/sensor_json.log", "a", encoding="utf-8") as f:
+        f.write(json.dumps(data)+'\n')
+
+    # test 형태로 파일 기록(한줄에 로그 1개씩) -> f-string 구성
+    # 파일명 ./sensor_logs/sensor_text.log
+    text = f"[{data["timestamp"]}] ID={data["sensor_id"]} |   TEMP:{data["temperature"]} |   HUMI:{data["humidity"]} |   STAT:{data["status"]}"
+    with open(f"{log_dir}/sensor_text.log", "a", encoding="utf-8") as f:
+        f.write(json.dumps(text)+'\n')
+
+    print(f"로그 발생 완료{data['timestamp']}")
+    
 
 # 4. 로그 발생기 가동
 def main():
-    pass
+    try:
+        while True:
+            generate_logs()
+            time.sleep(2) # 2초 대기(임의 설정)
+    except Exception:
+        print('종료 완료')
 
 # 5. 프로그램 시작
 if __name__ == '__main__':
